d6971dcb150781474bec50f1b5c612d52965ec70
DeGrandisLuca <luca.degrandis@altiliagroup.com>
2023-10-03 18:05:28 +0200

    minor fix

--- text_metrics_wrapper/preprocessing/preprocess_text_blocks.py
+++ text_metrics_wrapper/preprocessing/preprocess_text_blocks.py
@@ -2,11 +2,7 @@ from typing import List, Union
 from ..tokenizers.tokenizer import tokenizer
 
 
-def preprocess_text_blocks(
-    text_blocs: Union[List[str], List[List[str]]],
-    tokenizer_name: str,
-    **kwargs: dict
-) -> None:
+def preprocess_text_blocks(text_blocs: Union[List[str], List[List[str]]], tokenizer_name: str, **kwargs: dict) -> None:
     """
     Preprocesses the given text blocks using the specified tokenizer.
 
@@ -20,6 +16,7 @@ def preprocess_text_blocks(
     """
     selected_tokenizer = tokenizer(tokenizer_name)
 
+    text = text.replace("\n", " ").replace("  ", " ")
     preprocessed_text = selected_tokenizer.execute(text, **kwargs)
 
     return preprocessed_text
