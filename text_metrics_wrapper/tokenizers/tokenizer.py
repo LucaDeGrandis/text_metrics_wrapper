c5d99e73ffa2a6be32ad2cbb5c6fcf907bd02ca2
DeGrandisLuca <luca.degrandis@altiliagroup.com>
2023-10-10 18:22:22 +0200

    asdfasdf

--- text_metrics_wrapper/tokenizers/tokenizer.py
+++ text_metrics_wrapper/tokenizers/tokenizer.py
@@ -40,6 +40,7 @@ class tokenizer:
             if isinstance(block, text_block):
                 tokenized_sentences.append(self.tokenizer(block.text))
             elif isinstance(block, str):
+                print(self.tokenizer)
                 tokenized_sentences.append(self.tokenizer(block))
 
         return tokenized_sentences
