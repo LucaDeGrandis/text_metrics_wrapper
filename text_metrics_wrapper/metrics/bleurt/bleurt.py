5a1f906b8c304addf52068e9ad33a826524c0c3a
DeGrandisLuca <luca.degrandis@altiliagroup.com>
2023-10-18 12:59:57 +0200

    fix to bleurt

--- text_metrics_wrapper/metrics/bleurt/bleurt.py
+++ text_metrics_wrapper/metrics/bleurt/bleurt.py
@@ -38,8 +38,11 @@ def Bleurt(
 
     assert kwargs["method"] in ["max", "avg"], "method must be either 'max' or 'avg'"
 
+    if isinstance(references[0], list):
+        references_post = [[x] for x in references]
+
     scores = []
-    for _index, (hyp, refs) in enumerate(zip(hypothesis, references)):
+    for _index, (hyp, refs) in enumerate(zip(hypothesis, references_post)):
         hyp_scores = []
         for ref in refs:
             hyp_scores.append(scorer.score(references=[ref], candidates=[hyp])[0])
