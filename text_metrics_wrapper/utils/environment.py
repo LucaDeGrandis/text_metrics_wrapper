58c8254025a84d9e19176806913153479a330797
DeGrandisLuca <luca.degrandis@altiliagroup.com>
2023-10-18 11:05:29 +0200

    minor_fix

--- text_metrics_wrapper/utils/environment.py
+++ text_metrics_wrapper/utils/environment.py
@@ -16,7 +16,6 @@ def load_environment_variables(
 def set_logger(log_file_path):
     logger = logging.getLogger()
     f_handler = logging.FileHandler(filename=log_file_path)
-    file_handler.setLevel(logging.INFO)
     logger.addHandler(f_handler)
     logger.setLevel(logging.INFO)
     return logger
